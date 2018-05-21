3c6ee271ee461d17b4fb2455a63073e6ff1b6036
Galina Moskalova <galinamoskalova@inbox.lv>
2018-05-21 17:36:44 +0300

    login

--- classes/Template_class.php
+++ classes/Template_class.php
@@ -12,74 +12,75 @@ class Template_class{
     <link href="./libs/lightbox/css/lightbox.css" rel="stylesheet">';
     }
 
-    static function getmenu(){
-
-if(!isset($_SESSION['nick'])){
-    echo '<li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Profils<span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="./registracija.php">Reģistrēties</a></li>  
-                    </ul>
+    static function getmenu()
+    {
+        if(!isset($_SESSION['nick'])){
+            echo '<li class="dropdown">
+                <a class="dropdown-toggle" data-toggle="dropdown" href="#">Profils<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="./registracija.php">Reģistrēties</a></li>  
+                </ul>
                 </li>';
-}
-else{
-    echo '<li><a href="./registracija.php">'.$_SESSION['username'].'</a></li>
-     <li><form method="post"><button class="btn btn-primary" name="i">Iziet</button></form></li>';
-    if(isset($_POST['i'])){
-        session_destroy();
-        header('location:index.php');
-    }
-    if($_SESSION['role']==4){
-        echo '<li><a href="./Lietotaji.php">Lietotāji</a></li>';
-    }
-}
+        }
+        else{
+            echo '<li><a href="./registracija.php">'.$_SESSION['username'].'</a></li>
+             <li><form method="post"><button class="btn btn-primary" name="i">Iziet</button></form></li>';
+            if(isset($_POST['i'])){
+                session_destroy();
+                header('location:index.php');
+            }
+            if($_SESSION['role']==4){
+                echo '<li><a href="./Lietotaji.php">Lietotāji</a></li>';
+            }
+        }
     }
 
     static function getNav(){
         echo '<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="logo">
-      <img src="img/doglogo.jpg">
-    </div>
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span> 
-      </button>
-    </div>
-    <div class="collapse navbar-collapse navbar-right" id="myNavbar">
-      <ul class="nav navbar-nav">
-          <li><a href="index.php">Sākums</a></li>
-          <li><a href="galerija.php">Galerija</a></li>
-          <li><a href="pakalpojumi.php">Pakalpojumi</a></li>
-          <li><a href="kontakti.php">Kontakti</a></li>
-           <li><a href="Lietotaji.php">Lietotaji</a></li>
-      </ul> 
-       <ul>
-            <li><a href="">Azmirsi paroli?</a></li>
-             <li><a href="./registracija.php">Reģistrēties</a></li>
-       </ul>
-    </div>
-     <form action="index.php" method="post">
-      <div class="login"  >        
-                        <div class="form-group">
-                            <label for="5">Parole:</label>
-                            <input id="5" type="text" class="form-control"   name="parole">
-                        </div>
-                        <div class="form-group">
+              <div class="container-fluid">
+                <div class="logo">
+                  <img src="img/doglogo.jpg">
+                </div>
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span> 
+                  </button>
+                </div>
+                <div class="collapse navbar-collapse navbar-right" id="myNavbar">
+                  <ul class="nav navbar-nav">
+                      <li><a href="index.php">Sākums</a></li>
+                      <li><a href="galerija.php">Galerija</a></li>
+                      <li><a href="pakalpojumi.php">Pakalpojumi</a></li>
+                      <li><a href="kontakti.php">Kontakti</a></li>
+                       <li><a href="Lietotaji.php">Lietotaji</a></li>
+                  </ul> 
+                  
+                </div>
+               </div>
+               <div>
+                 <form action="index.php" method="post" class="navbar-form navbar-right">
+                        <div class="form-group aut">
                             <label for="6">Epasts:</label>
                             <input id="6" type="text" class="form-control"  name="epasts">
                         </div>
+                        <div class="form-group aut"> 
+                            <label for="5">Parole:</label>
+                            <input id="5" type="text" class="form-control"   name="parole">
+                        </div>
                         <div>
-                            <button type="submit" class="btn btn-primary" name="sutit">Ienākt</button>
-                        </div> 
-       </div>
-                    </form>
-                    
- 
-   </div>  
-</nav>';
+                            <button type="submit" class="btn" name="sutit">Ienākt</button>
+                        </div>
+                         <div class="reg">
+                             <a href="">Azmirsi paroli?</a>
+                         </div>
+                         <div class="reg">
+                             <a href="./registracija.php">Reģistrēties</a>
+                         </div>
+                 </form> 
+              </div>
+            </nav>';
     }
 }
 ?>
