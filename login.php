2a80628d4d3b1d901f5d180331f2e3e5d8afcd5e
aagarw30 <galinamoskalova@gmail.com>
2018-06-07 09:30:56 +0300

    qqqqaaaaa

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php require_once ("./classes/autoload.php");?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Dogsvet</title>
+    <?php Template_class::getLibs(); ?>
+</head>
+<body>
+<?php Template_class::getNav(); ?>
+<div class="container-fluid main-container">
+    <div class="row">
+        <div class="main">
+            <div  align="center">
+                <div class="col-sm-2"></div>
+                <div class="col-sm-8"> <h1>Dogs VET </h1>
+                    <div class="col-sm-2"></div>
+
+</body>
+</html>
